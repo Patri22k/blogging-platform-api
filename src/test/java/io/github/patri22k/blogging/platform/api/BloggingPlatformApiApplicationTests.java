7706cd436fcd34cd428371b51d0a123bebd7504c
Patri22k <bajzik.patrik6@gmail.com>
2025-07-01 11:48:19 +0200

    Initial commit

---
+++ src/test/java/io/github/patri22k/blogging/platform/api/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.patri22k.blogging.platform.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
